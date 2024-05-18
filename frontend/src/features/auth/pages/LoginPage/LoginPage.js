8ea53cb3c0d255e506744baa07a663a9270d08f9
TeaWithSalt <mbwlove@mail.ru>
2024-05-19 03:11:39 +0500

    feat: add auth background

--- frontend/src/features/auth/pages/LoginPage/LoginPage.js
+++ frontend/src/features/auth/pages/LoginPage/LoginPage.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
 import styles from './LoginPage.module.css'
 import {LoginForm} from '../../components/LoginForm/LoginForm';
-import {useRive} from "@rive-app/react-canvas";
+import {Alignment, Fit, Layout, useRive} from "@rive-app/react-canvas";
+import {Card} from "antd";
+import {ReactComponent as Logo} from "../../../../assets/images/Logo.svg";
 
 export function LoginPage() {
-    const navigate = useNavigate()
-
     const {rive, RiveComponent} = useRive({
-        src: './Train.riv',
+        src: './Train2.riv',
         stateMachines: "State Machine 1",
         autoplay: true,
+        layout: new Layout({fit: Fit.Cover, alignment: Alignment.Center})
     });
 
 
     return (
         <div className={styles.loginPage}>
-            <RiveComponent
-                className={styles.loginPage__logo}/>
-            <div className={styles.loginPage__titleContainer}>
-                <h1 className={styles.loginPage__title}>С возвращением!</h1>
-                <p className={styles.loginPage__description}>РЖД.разговоры - сервис для прослушивания РПЛ <br/>и фиксации нарушений требований регламента</p>
-            </div>
-            <LoginForm/>
+            <RiveComponent className={styles.riveAnimation}/>
+            <Card className={styles.loginCard}>
+                <Logo className={styles.logo}/>
+
+                <div className={styles.loginPage__titleContainer}>
+                    <p className={styles.loginPage__description}>
+                        Cервис для прослушивания РПЛ <br/>и
+                        фиксации нарушений требований регламента
+                    </p>
+                </div>
+
+                <div className={styles.loginForm__container}>
+                    <LoginForm/>
+                </div>
+            </Card>
         </div>
     )
 }
