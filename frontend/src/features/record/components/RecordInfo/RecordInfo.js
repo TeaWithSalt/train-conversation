d93b708e81a1c829e34f96a99b68f550e4be1150
TeaWithSalt <mbwlove@mail.ru>
2024-05-19 04:26:12 +0500

    feat: add player

--- frontend/src/features/record/components/RecordInfo/RecordInfo.js
+++ frontend/src/features/record/components/RecordInfo/RecordInfo.js
@@ -5,33 +5,6 @@ import styles from "./RecordInfo.module.css"
 import ReactAudioPlayer from "react-audio-player";
 
 export default function RecordInfo(props) {
-    const navigate = useNavigate()
-
-    const recordInfo = {
-        id: 3,
-        situation: {
-            id: 1,
-            name: "Прием поезда по регистрируемому приказу ДСП станции"
-        },
-        date: "2024-05-19T17:30:52.255Z",
-        participants: [
-            {
-                id: 1,
-                name: "Галимзянов Айнур",
-                role: "Диспетчер",
-                avatarSrc: "https://i.pravatar.cc/150?img=12"
-            },
-            {
-                id: 2,
-                name: "Килязова Юния",
-                role: "Машинист",
-                avatarSrc: "https://i.pravatar.cc/150?img=44"
-            }
-        ],
-        audio: "https://s10myt.storage.yandex.net/get-mp3/8afdfa7cf1cc82c057c8916413d1fc1c/000618b115563f9b/rmusic/U2FsdGVkX18vcJsXdxYg1PeNSseXWmjGwV9rcw4Z8U3YfKl46_KfwuJMMqseUGf1HVjvIkekLNMYuP1VlOL_FywpT6nKAsMoIgJ8-c2-MjQ/339b493dd462de349f2384041d3cabfa4da5b916214265d2e933f8cfdf187630/29663?track-id=120362106&play=false"
-    }
-
-
     return (
         <Card>
             <h2>Информация о записи</h2>
@@ -55,10 +28,6 @@ export default function RecordInfo(props) {
                             ))
                         }
                     </div>
-                </div>
-                <div className={styles.recordInfo__item}>
-                    <p className={styles.recordInfo__item__title}>Аудиозапись переговоров</p>
-                    <ReactAudioPlayer src={props.record.audioSrc} controls/>
                 </div>
             </div>
         </Card>
--- frontend/src/features/record/pages/RecordPage/RecordPage.js
+++ frontend/src/features/record/pages/RecordPage/RecordPage.js
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef} from 'react';
 import styles from './RecordPage.module.css'
-import {Breadcrumb, Spin} from "antd";
+import {Breadcrumb, Button, Card, Spin} from "antd";
 import {Link, useParams} from "react-router-dom";
 import RecordRecognitionText from "../../components/RecordRecognitionText/RecordRecognitionText";
 import RecordInfo from "../../components/RecordInfo/RecordInfo";
@@ -8,16 +8,42 @@ import RecordSituationTable from "../../components/RecordSituationTable/RecordSi
 import {useDispatch} from "react-redux";
 import {getRecord} from "../../../../store/slices/recordSlice";
 import {useRecord} from "../../../../hooks/use-record";
+import {useWavesurfer} from '@wavesurfer/react'
+import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js'
+import {PauseOutlined, PlayCircleOutlined, RedoOutlined, UndoOutlined} from "@ant-design/icons";
 
 
 export function RecordPage(props) {
     const dispatch = useDispatch()
     const {recordId} = useParams()
     const record = useRecord()
+    const containerRef = useRef(null)
 
     useEffect(() => {
         dispatch(getRecord(recordId))
     }, [])
+
+    const {wavesurfer, isPlaying, currentTime} = useWavesurfer({
+        container: containerRef,
+        height: 50,
+        waveColor: '#a4a7e3',
+        progressColor: '#e21a1a',
+        url: record.record?.audioSrc,
+        plugins: useMemo(() => [Timeline.create()], []),
+    })
+
+    const onPlayPause = useCallback(() => {
+        wavesurfer && wavesurfer.playPause()
+    }, [wavesurfer])
+
+    const onSkip = useCallback(() => {
+        wavesurfer && wavesurfer.skip(5)
+    }, [wavesurfer])
+
+    const onBack = useCallback(() => {
+        wavesurfer && wavesurfer.skip(-5)
+    }, [wavesurfer])
+
 
     if (!record.record)
         return <Spin className={styles.spin}/>
@@ -40,6 +66,27 @@ export function RecordPage(props) {
                         table={record.record.situationTable}
                         content={JSON.parse(record.record.situationTableContent)}
                     />
+                    <Card>
+                        <div className={styles.playerContainer}>
+                            <h2 className={styles.player__title}>Аудиозапись переговоров</h2>
+                            <div className={styles.player} ref={containerRef}/>
+                            <div className={styles.player__buttons}>
+                                <Button
+                                    onClick={onBack}
+                                    icon={<UndoOutlined />}
+                                />
+                                <Button
+                                    onClick={onPlayPause}
+                                    icon={isPlaying ? <PauseOutlined/> : <PlayCircleOutlined/>}
+                                />
+                                <Button
+                                    onClick={onSkip}
+                                    icon={<RedoOutlined />}
+                                />
+                            </div>
+
+                        </div>
+                    </Card>
                     <RecordRecognitionText recognitionChats={record.record.recognition_texts}/>
                 </div>
                 <div className={styles.recordPage__right}>
