06c57318fa00b84be9ccc211fedae86d28982f92
TeaWithSalt <mbwlove@mail.ru>
2024-05-19 10:22:32 +0500

    fix: change host

--- frontend/src/api/host.js
+++ frontend/src/api/host.js
@@ -2,6 +2,6 @@ let HOST;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
     HOST = 'http://localhost:5000/api';
 else
-    HOST = 'https://d5d710csp8btpmh27bp8.apigw.yandexcloud.net';
+    HOST = 'https://d5d710csp8btpmh27bp8.apigw.yandexcloud.net/api';
 
 export default HOST;
